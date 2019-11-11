d714fc414792fea14fb8912bb1d9d3f42b0f0535
kortirso <kortirso@gmail.com>
2019-11-11 09:10:02 +0100

    fix

--- app/models/subscribe.rb
+++ app/models/subscribe.rb
@@ -1,6 +1,6 @@
 # Represents event subscribes
 class Subscribe < ApplicationRecord
-  #enum status: { rejected: 0, unknown: 1, signed: 2, approved: 3 }
+  enum status: { rejected: 0, unknown: 1, signed: 2, approved: 3 }
 
   belongs_to :event
   belongs_to :character
