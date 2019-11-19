f30972ed579c995f6b3896ca556a74e3eb55919f
kortirso <kortirso@gmail.com>
2019-11-19 21:39:19 +0100

    ignore ActionController::InvalidAuthenticityToken for oauth requests

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'email identify'
 end
+
+token_verifier = OmniAuth.config.before_request_phase
+OmniAuth.config.before_request_phase = proc do |env|
+  begin
+    token_verifier&.call(env)
+  rescue ActionController::InvalidAuthenticityToken => e
+    puts '----'
+  end
+end
