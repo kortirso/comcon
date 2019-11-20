df9c208ed1175af0f6d66e5f5d654704cab9fed5
kortirso <kortirso@gmail.com>
2019-11-20 07:48:12 +0100

    fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,18 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
-    provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'email identify', callback_url: 'http://guild-hall.org/users/auth/discord/callback'
+    provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'email identify', callback_url: 'https://guild-hall.org/users/auth/discord/callback'
   else
-    provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'email identify'
+    provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'email identify', callback_url: 'http://localhost:5000/users/auth/discord/callback'
   end
 end
 
 token_verifier = OmniAuth.config.before_request_phase
 OmniAuth.config.before_request_phase = proc do |env|
   begin
+    raise ActionController::InvalidAuthenticityToken
     token_verifier&.call(env)
   rescue ActionController::InvalidAuthenticityToken => e
     puts '----'
+    OmniAuth::FailureEndpoint.new(env).redirect_to_failure
   end
 end
