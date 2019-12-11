baedeac0bfae87fb01e69204ee5fc31c4ad5c4da
kortirso <kortirso@gmail.com>
2019-12-11 12:55:25 +0100

    fix migration

--- db/migrate/20191209195453_delete_empty_guild_roles.rb
+++ db/migrate/20191209195453_delete_empty_guild_roles.rb
@@ -1,5 +1,5 @@
 class DeleteEmptyGuildRoles < ActiveRecord::Migration[5.2]
   def change
-    GuildRole.includes(:character).where('guild_roles.guild_id = characters.guild_id').references(:characters).destroy_all
+    GuildRole.includes(:character).where.not('guild_roles.guild_id = characters.guild_id').references(:characters).destroy_all
   end
 end
