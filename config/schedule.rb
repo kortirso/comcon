c1a8bddfd3f9ff551851dc0f91b8510128c1eaaa
kortirso <kortirso@gmail.com>
2019-12-18 21:09:40 +0100

    remove reindexing from schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,3 @@
-# Rebuilding Indices
-every 10.minutes do
-  rake 'ts:rebuild'
-end
-
 # Send notifications for users about close event
 every 10.minutes do
   runner 'NotifyComingSoonEventsJob.perform_now'
