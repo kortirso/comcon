262ce1e9a53345351611248e47d8b5747cedce97
kortirso <kortirso@gmail.com>
2020-10-24 14:49:34 +0300

    fix for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.11.0'
+lock '~> 3.14.1'
 
 set :application, 'comcon'
 set :repo_url, 'git@github.com:kortirso/comcon.git'
